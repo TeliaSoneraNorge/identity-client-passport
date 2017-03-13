9793d579a703bb6a9d55331ad9c1d5f5b2c95af8
Agnethe Søraa <me@agnethesoraa.com>
2017-03-13 15:54:47 +0100

    Fix lint errors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 /* eslint-env node */
-/* eslint-disable no-var, func-names */
+/* eslint-disable no-var, func-names, no-console */
 import express from 'express';
-import {renderFile} from 'ejs';
+import { renderFile } from 'ejs';
 import bodyParser from 'body-parser';
 import passport from 'passport';
-import {Strategy} from 'passport-openidconnect';
+import { Strategy } from 'passport-openidconnect';
 import session from 'express-session';
 import _ from 'lodash';
 
@@ -16,8 +16,8 @@ app.engine('html', renderFile);
 app.set('views', __dirname);
 app.set('view engine', 'html');
 
-app.use(express.static(__dirname + '/../public'));
-app.use('/vendor', express.static(__dirname + '/../vendor'));
+app.use(express.static(`${__dirname}/../public`));
+app.use('/vendor', express.static(`${__dirname}/../vendor`));
 app.use(bodyParser.json());
 
 export const idpApiUrl = 'https://sandbox.login.telia.io/realms/telia/protocol/openid-connect';
@@ -26,26 +26,26 @@ const users = {};
 passport.use(
   'provider',
   new Strategy({
-    authorizationURL: idpApiUrl + '/auth',
-    tokenURL: idpApiUrl + '/token',
-    userInfoURL: idpApiUrl + '/userinfo',
+    authorizationURL: `${idpApiUrl}/auth`,
+    tokenURL: `${idpApiUrl}/token`,
+    userInfoURL: `${idpApiUrl}/userinfo`,
     clientID: 'addClientIDHere',
     clientSecret: 'AddClientSecretHere',
     callbackURL: 'http://localhost:3000/auth/provider/callback',
     scope: ['AddScopeHere'],
   },
-  function(accessToken, refreshToken, profile, done) {
+  (accessToken, refreshToken, profile, done) => {
     users[profile.preferred_username] = profile;
     done(null, profile);
   }
 ));
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done) => {
   console.log(user);
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
@@ -57,13 +57,13 @@ app.use(passport.session());
 app.get('/auth/provider', passport.authenticate('provider'));
 app.get('/auth/provider/callback',
   passport.authenticate('provider', { successRedirect: '/',
-                                      failureRedirect: '/login' }));
+    failureRedirect: '/login' }));
 
 app.get('/', (req, res) => {
   console.log(req.user);
-  res.render('index', {user: _.omit(req.user, "_raw")});
+  res.render('index', { user: _.omit(req.user, '_raw') });
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('Example client is listening on port 3000!'); // eslint-disable-line no-console
 });
